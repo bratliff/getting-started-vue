ee740b29c7f7e0ba4e6a22bae371e29bee19003e
Ben Ratliff <bratliff@amplify.com>
2016-12-13 17:20:09 -0500

    working components

--- js/main.js
+++ js/main.js
@@ -1,13 +1,41 @@
-var app3 = new Vue({
-  el: '#app-3',
-  data: {
-    seen: true
+// global registration
+
+ Vue.component('todo-item', {
+    template: '<li>This is a todo</li>'
+});
+
+new Vue({
+    el: '#app'
+});
+
+// local registration
+
+var Child = {
+  template: '<div>A custom component!</div>'
+}
+new Vue({
+  el: '#app2',
+  // ...
+  components: {
+    // <my-component> will only be available in parent's template
+    'my-component': Child
   }
 })
 
-var toggler = new Vue({
-  el: '#demo',
-  data: {
-    show: true
+// Data must be a function
+
+var data = { counter: 0 }
+Vue.component('simple-counter', {
+  template: '<button v-on:click="counter += 1">{{ counter }}</button>',
+  // data is technically a function, so Vue won't
+  // complain, but we return the same object
+  // reference for each component instance
+  data: function () {
+    return {
+        counter: 0
+    }
   }
+})
+new Vue({
+  el: '#example-2'
 })

90e0d0704b5e49c245e8d8976a51f3188604fd79
Ben Ratliff <bratliff@amplify.com>
2016-12-15 15:27:36 -0500

    added event to change state

--- js/main.js
+++ js/main.js
@@ -2,7 +2,12 @@ var app3 = new Vue({
   el: '#app-3',
   data: {
     seen: true
-  }
+    },
+    methods: {
+        hideIt: function() {
+            this.seen = false;
+        }
+    }
 })
 
 var toggler = new Vue({

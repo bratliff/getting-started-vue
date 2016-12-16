b556052afa1fa1ea5f9c070dd47090805f3feb0d
Benjamin Ratliff <ben@2bitcreative.com>
2016-12-16 13:48:16 -0500

    various event methods

--- js/main.js
+++ js/main.js
@@ -1,86 +1,52 @@
 var example1 = new Vue({
   el: '#example-1',
   data: {
-    items: [
-      { message: 'Foo' },
-      { message: 'Bar' }
-    ]
+    counter: 0
   }
 })
 
 var example2 = new Vue({
   el: '#example-2',
   data: {
-    parentMessage: 'Parent',
-    items: [
-      { message: 'Foo' },
-      { message: 'Bar' }
-    ]
+    name: 'Vue.js'
+  },
+  // define methods under the `methods` object
+  methods: {
+    greet: function (event) {
+      // `this` inside methods points to the Vue instance
+      alert('Hello ' + this.name + '!')
+      // `event` is the native DOM event
+      alert(event.target.tagName)
+    }
   }
 })
 
 var example3 = new Vue({
-    el: '#repeat-object',
-    data: {
-      object: {
-        FirstName: 'John',
-        LastName: 'Doe',
-        Age: 30
-      }
-    }
-})
-
-var example4 = new Vue({
-    el: '#example4'
-})
-
-Vue.component('todo-item', {
-  template: '\
-    <li>\
-      {{ title }}\
-      <button v-on:click="$emit(\'remove\')">X</button>\
-    </li>\
-  ',
-  props: ['title']
-})
-new Vue({
-  el: '#todo-list-example',
-  data: {
-    newTodoText: '',
-    todos: [
-      'Do the dishes',
-      'Take out the trash',
-      'Mow the lawn'
-    ]
-  },
+  el: '#example-3',
   methods: {
-    addNewTodo: function () {
-      this.todos.push(this.newTodoText)
-      this.newTodoText = ''
+    say: function (message) {
+      alert(message)
     }
   }
 })
 
-var example1 = new Vue({
-  el: '#example-1',
-  data: {
-    items: [
-      { message: 'Foo' },
-      { message: 'Bar' }
-    ]
+var keypress = new Vue({
+  el: '#keypress',
+  methods: {
+    say: function (message) {
+      alert("fuck yeah");
+    }
   }
 })
 
-var evenNumbers = new Vue({
-  el: '#even',
-  data: {
-  numbers: [ 1, 2, 3, 4, 5 ]
-},
-methods: {
-    even: function (numbers) {
-      return numbers.filter(function (number) {
-        return number % 2 === 0
-      })
+var altclick = new Vue({
+  el: '#modifier',
+  methods: {
+    regular: function () {
+      alert("regular click");
+    },
+    control: function() {
+        alert("special click");
     }
   }
 })

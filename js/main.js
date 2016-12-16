b7105982452c4ccb964965be9b85efc83d7b7adf
Benjamin Ratliff <ben@2bitcreative.com>
2016-12-16 14:22:58 -0500

    added input bindings

--- js/main.js
+++ js/main.js
@@ -1,52 +1,12 @@
-var example1 = new Vue({
-  el: '#example-1',
+new Vue({
+  el: '#demo',
   data: {
-    counter: 0
-  }
-})
-
-var example2 = new Vue({
-  el: '#example-2',
-  data: {
-    name: 'Vue.js'
-  },
-  // define methods under the `methods` object
-  methods: {
-    greet: function (event) {
-      // `this` inside methods points to the Vue instance
-      alert('Hello ' + this.name + '!')
-      // `event` is the native DOM event
-      alert(event.target.tagName)
-    }
-  }
-})
-
-var example3 = new Vue({
-  el: '#example-3',
-  methods: {
-    say: function (message) {
-      alert(message)
-    }
-  }
-})
-
-var keypress = new Vue({
-  el: '#keypress',
-  methods: {
-    say: function (message) {
-      alert("fuck yeah");
-    }
-  }
-})
-
-var altclick = new Vue({
-  el: '#modifier',
-  methods: {
-    regular: function () {
-      alert("regular click");
-    },
-    control: function() {
-        alert("special click");
-    }
+    msg      : 'hi!',
+    checked  : true,
+    picked   : 'one',
+    selected : 'two',
+    multiSelect: ['one', 'three'],
+    message : '',
+    checkedNames : []
   }
 })

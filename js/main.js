ffe5274a93bc9bb702b4757db721262dc1bcf7d1
Benjamin Ratliff <ben@2bitcreative.com>
2016-12-16 13:17:24 -0500

    list and looping and filtering  methods for vue

--- js/main.js
+++ js/main.js
@@ -1,24 +1,86 @@
-
-var toggler = new Vue({
-  el: '#demo',
+var example1 = new Vue({
+  el: '#example-1',
   data: {
-    ok: true
+    items: [
+      { message: 'Foo' },
+      { message: 'Bar' }
+    ]
+  }
+})
+
+var example2 = new Vue({
+  el: '#example-2',
+  data: {
+    parentMessage: 'Parent',
+    items: [
+      { message: 'Foo' },
+      { message: 'Bar' }
+    ]
+  }
+})
+
+var example3 = new Vue({
+    el: '#repeat-object',
+    data: {
+      object: {
+        FirstName: 'John',
+        LastName: 'Doe',
+        Age: 30
+      }
+    }
+})
+
+var example4 = new Vue({
+    el: '#example4'
+})
+
+Vue.component('todo-item', {
+  template: '\
+    <li>\
+      {{ title }}\
+      <button v-on:click="$emit(\'remove\')">X</button>\
+    </li>\
+  ',
+  props: ['title']
+})
+new Vue({
+  el: '#todo-list-example',
+  data: {
+    newTodoText: '',
+    todos: [
+      'Do the dishes',
+      'Take out the trash',
+      'Mow the lawn'
+    ]
   },
   methods: {
-    changestatus : function() {
-      this.ok = !this.ok;
+    addNewTodo: function () {
+      this.todos.push(this.newTodoText)
+      this.newTodoText = ''
     }
   }
 })
 
-var show = new Vue({
-  el: '#demo2',
+var example1 = new Vue({
+  el: '#example-1',
   data: {
-    ok: true
-  },
-  methods: {
-    changestatus : function() {
-      this.ok = !this.ok;
+    items: [
+      { message: 'Foo' },
+      { message: 'Bar' }
+    ]
+  }
+})
+
+var evenNumbers = new Vue({
+  el: '#even',
+  data: {
+  numbers: [ 1, 2, 3, 4, 5 ]
+},
+methods: {
+    even: function (numbers) {
+      return numbers.filter(function (number) {
+        return number % 2 === 0
+      })
     }
   }
 })
